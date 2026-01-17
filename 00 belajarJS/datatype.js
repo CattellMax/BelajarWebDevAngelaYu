e884210f6f8252ee96f37c910a6ac52161c49f7b
CattellMax <bhimonw1503@gmail.com>
2026-01-17 20:33:25 +0800

    Add initial JavaScript files for data types and Hello World examples

---
+++ 00 belajarJS/datatype.js
@@ -0,0 +1,12 @@
+// Data Types in JavaScript
+let name = "John Doe"; // String
+let age = 30; // Number
+let isStudent = true; // Boolean
+let hobbies = ["reading", "traveling", "coding"]; // Array
+let address = { // Object
+    street: "123 Main St",
+    city: "Anytown",
+    country: "USA"
+};
+let nullValue = null; // Null
+let undefinedValue; // Undefined
---
+++ 00 belajarJS/helloworld.js
@@ -0,0 +1,4 @@
+alert ("Hello World");
+alert ("Selamat Belajar JavaScript");
+console.log ("Ini adalah pesan dari console");
+console.log ("Semangat belajar JavaScript");
